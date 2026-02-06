9db4dbdfdf71e9d872fe465bf3896d9504660227
TianDongL <crazylu1654@gmail.com>
2026-02-06 14:59:49 +0800

    回撤代码

--- app/ui/src/main.tsx
+++ app/ui/src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './i18n';
-import './web-ipc'; // Initialize Web IPC Shim for Cloud/Browser support
-
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
