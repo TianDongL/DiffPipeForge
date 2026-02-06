ef79516862688177eaa054e9ba291689acb0c8d4
TianDongL <crazylu1654@gmail.com>
2026-02-06 14:45:47 +0800

    云端支持,修改转发

--- app/ui/src/main.tsx
+++ app/ui/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './i18n';
+import './web-ipc'; // Initialize Web IPC Shim for Cloud/Browser support
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
